32a152d8f116872a6f49f477367fe6f2c98a007b
RyanHazlewood0 <114792045+RyanHazlewood0@users.noreply.github.com>
2024-07-12 11:58:04 +1200

    installed PropTypes and used in Coin and layout to solve linting errors

--- src/app/coin/[coinId]/page.jsx
+++ src/app/coin/[coinId]/page.jsx
@@ -1,6 +1,13 @@
+import PropTypes from "prop-types";
+
 export default function Coin({params})  {
 return (
     <div>{params.coinId}</div>
 )
 }
 
+Coin.propTypes = {
+params: PropTypes.shape({
+    coinId: PropTypes.string.isRequired
+}).isRequired
+};
