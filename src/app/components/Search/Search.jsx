60d370b1fc45c4e4edb7d5fb9057ff0e94d82d93
RyanHazlewood0 <114792045+RyanHazlewood0@users.noreply.github.com>
2024-07-23 16:16:10 +1200

    created dropdown div which opens when input typed into. rendering links for coins which contain the search value

--- src/app/components/Search/Search.jsx
+++ src/app/components/Search/Search.jsx
@@ -1,32 +1,73 @@
+"use client";
 import { useState } from "react";
+import { useCoin } from "@/app/contexts/CoinProvider";
+import Link from "next/link";
 import styled from "styled-components";
+
+const SearchContainer = styled.div`
+  display: column;
+  width: 356px;
+`;
 
 const SearchInput = styled.input`
   background: #191925;
   padding: 10px 10px 10px 35px;
   border-radius: 6px;
+  width: 100%;
+`;
+
+const DropDown = styled.div`
+  margin-top: 5px;
+  position: absolute;
+  width: 100%;
+  background: #191925;
+  padding: 10px;
+  border-radius: 6px;
 `;
 
 const Search = () => {
   const [searchValue, setSearchValue] = useState("");
+  const [dropDownOpen, setDropDownOpen] = useState(false);
+  const { coins } = useCoin();
 
-  const handleChangeSearchValue = (e) => {
+  const handleSearch = (e) => {
     setSearchValue(e.target.value);
+    if (e.target.value.length !== 0) {
+      setDropDownOpen(true);
+    } else {
+      setDropDownOpen(false);
+    }
   };
 
-  const handleSearchSubmit = (e) => {
-    e.preventDefault;
+  const filteredCoins = coins.filter(
+    (coin) =>
+      coin.id.includes(searchValue) ||
+      coin.symbol.includes(searchValue) ||
+      coin.name.includes(searchValue)
+  );
+
+  const handleBlur = () => {
+    setDropDownOpen(false);
   };
 
   return (
-    <form onSubmit={handleSearchSubmit}>
+    <SearchContainer>
       <SearchInput
         type="text"
         placeholder="Search..."
         value={searchValue}
-        onChange={(e) => handleChangeSearchValue(e)}
+        onChange={(e) => handleSearch(e)}
       />
-    </form>
+      {dropDownOpen && (
+        <DropDown onBlur={handleBlur}>
+          {filteredCoins.map((coin) => (
+            <Link href={`/coin/${coin.id}`} key={coin.id}>
+              <p>{coin.name}</p>
+            </Link>
+          ))}
+        </DropDown>
+      )}
+    </SearchContainer>
   );
 };
 
