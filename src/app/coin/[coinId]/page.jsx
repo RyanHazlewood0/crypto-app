7724b10d83faa41c6479e0efbab4e904b5317f84
RyanHazlewood0 <114792045+RyanHazlewood0@users.noreply.github.com>
2024-07-31 10:50:46 +1200

    made thisCoin variable to find coin which has ID matching the params

--- src/app/coin/[coinId]/page.jsx
+++ src/app/coin/[coinId]/page.jsx
@@ -1,13 +1,22 @@
+"use client";
 import PropTypes from "prop-types";
+import { useCoin } from "@/app/contexts/CoinProvider";
 
-export default function Coin({params})  {
-return (
-    <div>{params.coinId}</div>
-)
+export default function Coin({ params }) {
+  const { coins } = useCoin();
+  const thisCoin = coins.find((coin) => coin.id === params.coinId);
+
+  return (
+    coins && (
+      <>
+        <p>{thisCoin.symbol}</p>
+      </>
+    )
+  );
 }
 
 Coin.propTypes = {
-params: PropTypes.shape({
-    coinId: PropTypes.string.isRequired
-}).isRequired
+  params: PropTypes.shape({
+    coinId: PropTypes.string.isRequired,
+  }).isRequired,
 };
