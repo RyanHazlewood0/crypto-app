db962324f0a0e067261f23a898e87a2dfb95b15c
RyanHazlewood0 <114792045+RyanHazlewood0@users.noreply.github.com>
2024-07-31 13:45:59 +1200

    added fetch request for relevent data point and store it in state. started rendering some of the data.

--- src/app/coin/[coinId]/page.jsx
+++ src/app/coin/[coinId]/page.jsx
@@ -1,7 +1,10 @@
 "use client";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Link from "next/link";
 import { useCoin } from "@/app/contexts/CoinProvider";
+import { useState, useEffect } from "react";
+import { abbreviateNumber } from "@/app/components/Table/helper-functions";
 
 const Container = styled.div`
   width: 100%;
@@ -11,57 +14,94 @@ const Header = styled.h1`
   font-size: 20px;
 `;
 
+const BackBtn = styled.div`
+  font-size: 20px;
+  margin-right: 15px;
+`;
+
+const HeaderAndBtnContainer = styled.div`
+  display: flex;
+`;
+
 const TopHalfContainer = styled.div`
   width: 100%;
   display: flex;
-  background: green;
   height: 420px;
+  justify-content: space-between;
+  margin-bottom: 100px;
 `;
 
 const BottomHalfContainer = styled.div`
   width: 100%;
   display: flex;
-  background: orange;
   height: 250px;
+  justify-content: space-between;
 `;
 
 const InfoContainerOne = styled.div`
-  width: 662px;
-  background: blue;
+  width: 692px;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const NameAndLinkContainer = styled.div`
   width: 305px;
   height: 333px;
-  background: gray;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const CoinNameContainer = styled.div`
   width: 305px;
   height: 265px;
   background: brown;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #1e1932;
+  border-radius: 12px;
 `;
 
 const PriceInfoContainer = styled.div`
   width: 355px;
   height: 333px;
   background: pink;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #1e1932;
+  border-radius: 12px;
 `;
 
 const CoinLinkContainer = styled.div`
   width: 305px;
   height: 52px;
   background: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #1e1932;
+  border-radius: 12px;
 `;
 
 const InfoContainerTwo = styled.div`
   width: 544px;
-  background: blue;
+  background: purple;
+  height: 420px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #1e1932;
+  border-radius: 12px;
 `;
 
 const InfoContainerThree = styled.div`
   width: 692px;
-  background: blue;
+  height: 250px;
 `;
 
 const DescriptionHeader = styled.h2`
@@ -74,53 +114,94 @@ const DescriptionText = styled.p`
 
 const InfoContainerFour = styled.div`
   width: 544px;
-  background: blue;
+  height: 204px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const LinkContainer = styled.div`
   width: 100%;
   height: 52px;
   background: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #1e1932;
+  border-radius: 12px;
 `;
 
 export default function Coin({ params }) {
+  const [hasError, setHasError] = useState(false);
+  const [coinData, setCoinData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const { coins } = useCoin();
-  const thisCoin = coins.find((coin) => coin.id === params.coinId);
+
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const thisCoin = coins.find((coin) => coin.id === params.coinId);
+      setHasError(false);
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `https://api.coingecko.com/api/v3/coins/${thisCoin.id}?localization=false&tickers=false&market_data=true&community_data=true&developer_data=false&sparkline=falsex_cg_pro_api_key=${apiKey}`
+        );
+        const fetchedData = await response.json();
+        setCoinData(fetchedData);
+        setIsLoading(false);
+      } catch {
+        setIsLoading(false);
+        setHasError(true);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading Data</p>;
+  }
+
+  if (hasError) {
+    return <p>Error loading data</p>;
+  }
 
   return (
-    thisCoin && (
+    coinData !== null && (
       <Container>
-        <Header>{thisCoin.name}</Header>
+        <HeaderAndBtnContainer>
+          <Link href="/">
+            <BackBtn>←</BackBtn>
+          </Link>
+          <Header>{coinData.name}</Header>
+        </HeaderAndBtnContainer>
         <TopHalfContainer>
           <InfoContainerOne>
             <NameAndLinkContainer>
               <CoinNameContainer>
-                <img src={thisCoin.image} style={{ width: "64=px" }} />
-                <h1 style={{ fontSize: "28px" }}>{thisCoin.name}</h1>
+                <img src={coinData.image.small} />
+                <h1 style={{ fontSize: "28px" }}>
+                  {coinData.name} ({coinData.symbol})
+                </h1>
               </CoinNameContainer>
-              <CoinLinkContainer>www.bitcoin.com</CoinLinkContainer>
+              <Link href={coinData.links.homepage[0]}>
+                <CoinLinkContainer>{coinData.links.homepage}</CoinLinkContainer>
+              </Link>
             </NameAndLinkContainer>
-            <PriceInfoContainer> price stuff</PriceInfoContainer>
+            <PriceInfoContainer>
+              <h1 style={{ fontSize: "36px", fontWeight: "bold" }}>
+                ${abbreviateNumber(coinData.market_data.current_price.usd)}
+              </h1>
+            </PriceInfoContainer>
           </InfoContainerOne>
           <InfoContainerTwo>bunch of price stuff</InfoContainerTwo>
         </TopHalfContainer>
         <BottomHalfContainer>
           <InfoContainerThree>
             <DescriptionHeader>Description</DescriptionHeader>
-            <DescriptionText>
-              There are many variations of passages of Lorem Ipsum available,
-              but the majority have suffered alteration in some form, by
-              injected humour, or randomised words which dont look even slightly
-              believable. If you are going to use a passage of Lorem Ipsum, you
-              need to be sure there isnt anything embarrassing hidden in the
-              middle of text. All the Lorem Ipsum generators on the Internet
-              tend to repeat predefined chunks as necessary, making this the
-              first true generator on the Internet. It uses a dictionary of over
-              200 Latin words, combined with a handful of model sentence
-              structures, to generate Lorem Ipsum which looks reasonable. The
-              generated Lorem Ipsum is therefore always free from repetition,
-              injected humour, or non-characteristic words etc.
-            </DescriptionText>
+            <DescriptionText>{coinData.description.en}</DescriptionText>
           </InfoContainerThree>
           <InfoContainerFour>
             <LinkContainer>www.google.com</LinkContainer>
