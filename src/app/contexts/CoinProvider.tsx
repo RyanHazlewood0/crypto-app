e556684557c55a75fcf46ec07e4f6b22c00a9120
RyanHazlewood0 <114792045+RyanHazlewood0@users.noreply.github.com>
2024-08-16 09:29:55 +1200

    added types in context file

--- src/app/contexts/CoinProvider.tsx
+++ src/app/contexts/CoinProvider.tsx
@@ -2,9 +2,37 @@
 import { createContext, useContext } from "react";
 import { useState } from "react";
 
-const CoinContext = createContext();
+interface Coin {
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  fully_diluted_valuation: number;
+  total_volume: number;
+  price_change_percent_24h: number;
+  circulating_supply: number;
+  total_supply: number;
+  price_change_percentage_1h_in_currency: number;
+  price_change_percentage_24h_in_currency: number;
+  price_change_percentage_7d_in_currency: number;
+  sparkline_in_7d: {
+    price: number[];
+  };
+}
 
-export const useCoin = () => {
+interface CoinContextType {
+  coins: Coin[];
+  selectedBtn: string;
+  setCoins: (value: Coin[]) => void;
+  setSelectedBtn: (value: string) => void;
+}
+
+const CoinContext = createContext<CoinContextType | null>(null);
+
+export const useCoin = (): CoinContextType => {
   const value = useContext(CoinContext);
   return value;
 };
@@ -14,8 +42,8 @@ type useCoinProps = {
 };
 
 export const CoinProvider = ({ children }: useCoinProps) => {
-  const [coins, setCoins] = useState([]);
-  const [selectedBtn, setSelectedBtn] = useState("Coins");
+  const [coins, setCoins] = useState<Coin[]>([]);
+  const [selectedBtn, setSelectedBtn] = useState<string>("Coins");
 
   return (
     <CoinContext.Provider
