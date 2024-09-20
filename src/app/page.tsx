4c311fe12b154958024e91f6d09760881a8d5570
RyanHazlewood0 <114792045+RyanHazlewood0@users.noreply.github.com>
2024-09-20 13:28:21 +1200

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,19 @@ import CoinsAndConverterBtns from "./components/CoinsAndConverterBtns/CoinsAndCo
 import { useState } from "react";
 import { CoinTypes } from "types";
 import MobileButtons from "./components/MobileButtons/MobileButtons";
+import useWindowSize from "windowSizeHook";
+import { breakpoints } from "breakpoints";
 
 export default function Home() {
   const [selectedCoin, setSelectedCoin] = useState<CoinTypes | null>(null);
   const [timeFrameSelected, setTimeFrameSelected] = useState("1M");
   const [dayCount, setDayCount] = useState("30");
 
+  const size = useWindowSize();
+
   return (
     <>
-      <CoinsAndConverterBtns />
+      {size.width > parseInt(breakpoints.mobile) && <CoinsAndConverterBtns />}
       <Carousel setSelectedCoin={setSelectedCoin} selectedCoin={selectedCoin} />
       <HomePageCharts selectedCoin={selectedCoin} dayCount={dayCount} />
       <HomeChartTimeSelect
