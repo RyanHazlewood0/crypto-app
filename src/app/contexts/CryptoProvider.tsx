d7d4bdcf9e9670b6bf66480073d7bea68b71b98c
RyanHazlewood0 <ryanhazlewood0@gmail.com>
2025-11-10 22:13:53 +1300

    just fetching one lot of 250 coins instead of four in cryptoprovider

--- src/app/contexts/CryptoProvider.tsx
+++ src/app/contexts/CryptoProvider.tsx
@@ -110,16 +110,8 @@ export const CryptoProvider = ({ children }: useCryptoContextProps) => {
       const one = await api(
         `https://pro-api.coingecko.com/api/v3/coins/markets?vs_currency=${fiatCurrency}&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d&x_cg_pro_api_key=${apiKey}`
       );
-      const two = await api(
-        `https://pro-api.coingecko.com/api/v3/coins/markets?vs_currency=${fiatCurrency}&order=market_cap_desc&per_page=250&page=2&sparkline=true&price_change_percentage=1h%2C24h%2C7d&x_cg_pro_api_key=${apiKey}`
-      );
-      const three = await api(
-        `https://pro-api.coingecko.com/api/v3/coins/markets?vs_currency=${fiatCurrency}&order=market_cap_desc&per_page=250&page=3&sparkline=true&price_change_percentage=1h%2C24h%2C7d&x_cg_pro_api_key=${apiKey}`
-      );
-      const four = await api(
-        `https://pro-api.coingecko.com/api/v3/coins/markets?vs_currency=${fiatCurrency}&order=market_cap_desc&per_page=250&page=4&sparkline=true&price_change_percentage=1h%2C24h%2C7d&x_cg_pro_api_key=${apiKey}`
-      );
-      setCoins([...one, ...two, ...three, ...four]);
+
+      setCoins([...one]);
     };
     fetchData();
   }, [fiatCurrency]);
