92ee62e8f0836a6e978defbb1cb586c9e3f3bc48
RyanHazlewood0 <ryanhazlewood0@gmail.com>
2025-11-13 13:29:03 +1300

    coin page fully responsive

--- src/app/coin/[coinId]/page.tsx
+++ src/app/coin/[coinId]/page.tsx
@@ -98,7 +98,7 @@ export default function Coin({ params }: CoinProps) {
 
   return (
     <>
-      <div className={"w-full flex flex-col justify-center gap-[15px]"}>
+      <div className={"w-full flex flex-col justify-center gap-[15px] pb-5"}>
         {thisCoinData !== null && (
           <>
             <div className={"flex mb-2.5"}>
@@ -107,27 +107,219 @@ export default function Coin({ params }: CoinProps) {
               </Link>
               <h1 className={"text-[20px]"}>{thisCoinData.name}</h1>
             </div>
-            <div
-              className={`flex flex-col justify-center items-center w-full px-0 py-[10%] rounded-lg gap-5 ${
-                theme === "light" ? "bg-white" : "bg-[#1e1932]"
-              } `}
-            >
-              <img src={thisCoinData.image.small} />
-              <h1 style={{ fontSize: "28px" }}>
-                {thisCoinData.name} ({thisCoinData.symbol})
-              </h1>
+            <div className="flex flex-col md:flex-row gap-5">
               <div
-                className="ml-auto mr-auto cursor-pointer"
-                onClick={handleStarClick}
+                className={`flex flex-col justify-center items-center w-full px-0 py-[10%] rounded-lg gap-5 ${
+                  theme === "light" ? "bg-white" : "bg-[#1e1932]"
+                } `}
               >
-                <Star
-                  watchListCoins={watchListCoins}
-                  thisCoinData={thisCoinData}
-                />
+                <img src={thisCoinData.image.small} />
+                <h1 style={{ fontSize: "28px" }}>
+                  {thisCoinData.name} ({thisCoinData.symbol})
+                </h1>
+                <div
+                  className="ml-auto mr-auto cursor-pointer"
+                  onClick={handleStarClick}
+                >
+                  <Star
+                    watchListCoins={watchListCoins}
+                    thisCoinData={thisCoinData}
+                  />
+                </div>
+              </div>
+              <div
+                className={`w-full flex py-2.5 items-center justify-center rounded-lg px-2.5 md:hidden ${
+                  theme === "light" ? "bg-white" : "bg-[#1e1932]"
+                }`}
+              >
+                <a
+                  href={thisCoinData.links.homepage[0]}
+                  style={{ marginRight: "10px" }}
+                  className={"truncate"}
+                >
+                  {thisCoinData.links.homepage[0]}
+                </a>
+                <div onClick={handleCopyTrue} style={{ cursor: "pointer" }}>
+                  <CopyIcon />
+                </div>
+                {copyClicked === true && (
+                  <div
+                    className={
+                      "fixed top‑1/2 left‑1/2 transform ‑translate‑x‑1/2 ‑translate‑y‑1/2 bg‑black text‑white rounded‑[6px] p‑[10px]"
+                    }
+                  >
+                    Link has been copied!
+                  </div>
+                )}
+              </div>
+              <div
+                className={`w-full flex flex-col justify-center items-center rounded-[12px] gap-2.5 py-2.5 ${
+                  theme === "light" ? "bg-white" : "bg-[#1e1932]"
+                }`}
+              >
+                <h1 style={{ fontSize: "36px", fontWeight: "bold" }}>
+                  $
+                  {abbreviateNumber(
+                    thisCoinData.market_data.current_price[fiatCurrency]
+                  )}
+                </h1>
+                <div>
+                  <div className={"flex flex-col w-full gap-[20px]"}>
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "15px",
+                        marginBottom: "7px",
+                      }}
+                    >
+                      <GreenArrow />
+                      <p style={{ fontSize: "16px" }}>All time high:</p>
+                      <p style={{ fontSize: "20px" }}>
+                        $
+                        {abbreviateNumber(
+                          thisCoinData.market_data.ath[fiatCurrency]
+                        )}
+                      </p>{" "}
+                    </div>
+                    <p
+                      className={`text-[14px] text-center ${
+                        theme === "light" ? "text-[#353570]" : "text-[#b9b9ba]"
+                      }`}
+                    >
+                      {formatDate(
+                        thisCoinData.market_data.ath_date[fiatCurrency]
+                      )}
+                    </p>
+                  </div>
+                  <div>
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "15px",
+                        marginBottom: "7px",
+                      }}
+                    >
+                      <RedArrow />
+                      <p style={{ fontSize: "16px" }}>All time low:</p>
+                      <p style={{ fontSize: "20px" }}>
+                        $
+                        {abbreviateNumber(
+                          thisCoinData.market_data.atl[fiatCurrency]
+                        )}
+                      </p>{" "}
+                    </div>
+                    <p
+                      className={`text-[14px] text-center ${
+                        theme === "light" ? "text-[#353570]" : "text-[#b9b9ba]"
+                      }`}
+                    >
+                      {formatDate(
+                        thisCoinData.market_data.atl_date[fiatCurrency]
+                      )}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div
+                className={
+                  "w-full hidden lg:flex flex-col justify-center items-center rounded-[12px]"
+                }
+              >
+                <div
+                  className={`flex w-full justify-between items-center py-[5px] px-[15px] flex-1 ${
+                    theme === "light" ? "bg-white" : "bg-[#1e1932]"
+                  } rounded-t-[12px]`}
+                >
+                  <div className={"flex gap-2.5 text-[16px]"}>
+                    <RoundIcon />
+                    <p>Market Cap</p>
+                  </div>
+                  <div className={"flex items-center py-2.5 px-0"}>
+                    <div className={"text-[20px]"}>
+                      $
+                      {abbreviateNumber(
+                        thisCoinData.market_data.market_cap[fiatCurrency]
+                      )}
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className={`flex w-full text-[14px] justify-between items-center py-[5px] px-[15px] flex-1 ${
+                    theme === "light" ? "bg-white" : "bg-[#1e1932]"
+                  }`}
+                >
+                  <div className={"flex gap-2.5 text-[16px]"}>
+                    <RoundIcon />
+                    <p>Fully Diluted Valuation</p>
+                  </div>
+                  <div className={"flex items-center py-2.5 px-0"}>
+                    <p className={"text-[20px]"}>
+                      $
+                      {abbreviateNumber(
+                        thisCoinData.market_data.fully_diluted_valuation[
+                          fiatCurrency
+                        ]
+                      )}
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className={`flex w-full text-[14px] justify-between items-center py-[5px] px-[15px] flex-1 ${
+                    theme === "light" ? "bg-white" : "bg-[#1e1932]"
+                  }`}
+                >
+                  <div className={"flex gap-2.5 text-[16px]"}>
+                    <RoundIcon />
+                    <p>Volume 24h</p>
+                  </div>
+                  <td className={"flex items-center py-2.5 px-0"}>
+                    <p className={"text-[20px]"}>
+                      $
+                      {abbreviateNumber(
+                        thisCoinData.market_data.total_volume[fiatCurrency]
+                      )}
+                    </p>
+                  </td>
+                </div>
+                <div
+                  className={`flex w-full text-[14px] justify-between items-center py-[5px] px-[15px] flex-1 ${
+                    theme === "light" ? "bg-white" : "bg-[#1e1932]"
+                  }`}
+                >
+                  <div className={"flex gap-2.5 text-[16px]"}>
+                    <RoundIcon />
+                    <p>Circulating Supply</p>
+                  </div>
+                  <div className={"flex items-center py-2.5 px-0"}>
+                    <p className={"text-[20px]"}>
+                      {abbreviateNumber(
+                        thisCoinData.market_data.circulating_supply
+                      )}
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className={`flex w-full text-[14px] justify-between items-center py-[5px] px-[15px] rounded-b-[12px] flex-1 ${
+                    theme === "light" ? "bg-white" : "bg-[#1e1932]"
+                  }`}
+                >
+                  <div className={"flex gap-2.5 text-[16px]"}>
+                    <RoundIcon />
+                    <p>Total Supply</p>
+                  </div>
+                  <div className={"flex items-center py-2.5 px-0"}>
+                    <p className={"text-[20px]"}>
+                      {abbreviateNumber(thisCoinData.market_data.total_supply)}
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
+
             <div
-              className={`w-full flex py-2.5 items-center justify-center rounded-lg px-2.5 ${
+              className={`hidden w-full md:flex py-2.5 items-center justify-center rounded-lg px-2.5 ${
                 theme === "light" ? "bg-white" : "bg-[#1e1932]"
               }`}
             >
@@ -151,77 +343,7 @@ export default function Coin({ params }: CoinProps) {
                 </div>
               )}
             </div>
-            <div
-              className={`w-full flex flex-col justify-center items-center rounded-[12px] gap-2.5 py-2.5 ${
-                theme === "light" ? "bg-white" : "bg-[#1e1932]"
-              }`}
-            >
-              <h1 style={{ fontSize: "36px", fontWeight: "bold" }}>
-                $
-                {abbreviateNumber(
-                  thisCoinData.market_data.current_price[fiatCurrency]
-                )}
-              </h1>
-              <div>
-                <div className={"flex flex-col w-full gap-[20px]"}>
-                  <div
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      gap: "15px",
-                      marginBottom: "7px",
-                    }}
-                  >
-                    <GreenArrow />
-                    <p style={{ fontSize: "16px" }}>All time high:</p>
-                    <p style={{ fontSize: "20px" }}>
-                      $
-                      {abbreviateNumber(
-                        thisCoinData.market_data.ath[fiatCurrency]
-                      )}
-                    </p>{" "}
-                  </div>
-                  <p
-                    className={`text-[14px] text-center ${
-                      theme === "light" ? "text-[#353570]" : "text-[#b9b9ba]"
-                    }`}
-                  >
-                    {formatDate(
-                      thisCoinData.market_data.ath_date[fiatCurrency]
-                    )}
-                  </p>
-                </div>
-                <div>
-                  <div
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      gap: "15px",
-                      marginBottom: "7px",
-                    }}
-                  >
-                    <RedArrow />
-                    <p style={{ fontSize: "16px" }}>All time low:</p>
-                    <p style={{ fontSize: "20px" }}>
-                      $
-                      {abbreviateNumber(
-                        thisCoinData.market_data.atl[fiatCurrency]
-                      )}
-                    </p>{" "}
-                  </div>
-                  <p
-                    className={`text-[14px] text-center ${
-                      theme === "light" ? "text-[#353570]" : "text-[#b9b9ba]"
-                    }`}
-                  >
-                    {formatDate(
-                      thisCoinData.market_data.atl_date[fiatCurrency]
-                    )}
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className={"w-full"}>
+            <div className={"w-full lg:hidden"}>
               <table className={"w-full"}>
                 <tr
                   className={`flex w-full justify-between items-center py-[5px] px-[15px] ${
@@ -313,82 +435,86 @@ export default function Coin({ params }: CoinProps) {
                 </tr>
               </table>
             </div>
-            <p className={"text-[24px]"}>Description</p>
-            <p className={"text-14px"}>{thisCoinData.description.en}</p>
-            <div
-              className={`w-full flex py-2.5 items-center justify-center rounded-lg px-2.5 ${
-                theme === "light" ? "bg-white" : "bg-[#1e1932]"
-              }`}
-            >
-              <a
-                style={{ marginRight: "10px" }}
-                href={thisCoinData.links.blockchain_site[0]}
-                className={"truncate"}
-              >
-                {thisCoinData.links.blockchain_site[0]}
-              </a>
-              <div onClick={handleCopyTrue} style={{ cursor: "pointer" }}>
-                <CopyIcon />
-              </div>
-              {copyClicked === true && (
+            <div className="flex flex-col lg:flex-row gap-[15px]">
+              <p className={"text-[24px] lg:hidden"}>Description</p>
+              <p className={"text-14px"}>{thisCoinData.description.en}</p>
+              <div className={"flex flex-col gap-5 mt-auto"}>
                 <div
-                  className={
-                    "fixed top‑1/2 left‑1/2 transform ‑translate‑x‑1/2 ‑translate‑y‑1/2 bg‑black text‑white rounded‑[6px] p‑[10px]"
-                  }
+                  className={`w-full flex py-2.5 items-center justify-center rounded-lg px-2.5 ${
+                    theme === "light" ? "bg-white" : "bg-[#1e1932]"
+                  }`}
                 >
-                  Link has been copied!
+                  <a
+                    style={{ marginRight: "10px" }}
+                    href={thisCoinData.links.blockchain_site[0]}
+                    className={"truncate"}
+                  >
+                    {thisCoinData.links.blockchain_site[0]}
+                  </a>
+                  <div onClick={handleCopyTrue} style={{ cursor: "pointer" }}>
+                    <CopyIcon />
+                  </div>
+                  {copyClicked === true && (
+                    <div
+                      className={
+                        "fixed top‑1/2 left‑1/2 transform ‑translate‑x‑1/2 ‑translate‑y‑1/2 bg‑black text‑white rounded‑[6px] p‑[10px]"
+                      }
+                    >
+                      Link has been copied!
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-            <div
-              className={`w-full flex py-2.5 items-center justify-center rounded-lg px-2.5 ${
-                theme === "light" ? "bg-white" : "bg-[#1e1932] "
-              }`}
-            >
-              <a
-                style={{ marginRight: "10px" }}
-                href={thisCoinData.links.blockchain_site[1]}
-                className={"truncate"}
-              >
-                {thisCoinData.links.blockchain_site[1]}
-              </a>
-              <div onClick={handleCopyTrue} style={{ cursor: "pointer" }}>
-                <CopyIcon />
-              </div>
-              {copyClicked === true && (
                 <div
-                  className={
-                    "fixed top‑1/2 left‑1/2 transform ‑translate‑x‑1/2 ‑translate‑y‑1/2 bg‑black text‑white rounded‑[6px] p‑[10px]"
-                  }
+                  className={`w-full flex py-2.5 items-center justify-center rounded-lg px-2.5 ${
+                    theme === "light" ? "bg-white" : "bg-[#1e1932] "
+                  }`}
                 >
-                  Link has been copied!
+                  <a
+                    style={{ marginRight: "10px" }}
+                    href={thisCoinData.links.blockchain_site[1]}
+                    className={"truncate"}
+                  >
+                    {thisCoinData.links.blockchain_site[1]}
+                  </a>
+                  <div onClick={handleCopyTrue} style={{ cursor: "pointer" }}>
+                    <CopyIcon />
+                  </div>
+                  {copyClicked === true && (
+                    <div
+                      className={
+                        "fixed top‑1/2 left‑1/2 transform ‑translate‑x‑1/2 ‑translate‑y‑1/2 bg‑black text‑white rounded‑[6px] p‑[10px]"
+                      }
+                    >
+                      Link has been copied!
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-            <div
-              className={`w-full flex py-2.5 items-center justify-center rounded-lg px-2.5 ${
-                theme === "light" ? "bg-white" : "bg-[#1e1932]"
-              }`}
-            >
-              <a
-                style={{ marginRight: "10px" }}
-                href={thisCoinData.links.blockchain_site[2]}
-                className={"truncate"}
-              >
-                {thisCoinData.links.blockchain_site[2]}
-              </a>
-              <div onClick={handleCopyTrue} style={{ cursor: "pointer" }}>
-                <CopyIcon />
-              </div>
-              {copyClicked === true && (
                 <div
-                  className={
-                    "fixed top‑1/2 left‑1/2 transform ‑translate‑x‑1/2 ‑translate‑y‑1/2 bg‑black text‑white rounded‑[6px] p‑[10px]"
-                  }
+                  className={`w-full flex py-2.5 items-center justify-center rounded-lg px-2.5 ${
+                    theme === "light" ? "bg-white" : "bg-[#1e1932]"
+                  }`}
                 >
-                  Link has been copied!
+                  <a
+                    style={{ marginRight: "10px" }}
+                    href={thisCoinData.links.blockchain_site[2]}
+                    className={"truncate"}
+                  >
+                    {thisCoinData.links.blockchain_site[2]}
+                  </a>
+                  <div onClick={handleCopyTrue} style={{ cursor: "pointer" }}>
+                    <CopyIcon />
+                  </div>
+                  {copyClicked === true && (
+                    <div
+                      className={
+                        "fixed top‑1/2 left‑1/2 transform ‑translate‑x‑1/2 ‑translate‑y‑1/2 bg‑black text‑white rounded‑[6px] p‑[10px]"
+                      }
+                    >
+                      Link has been copied!
+                    </div>
+                  )}
                 </div>
-              )}
+              </div>
             </div>
           </>
         )}
